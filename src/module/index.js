9f20ca9b36240cfb8bfa9f6c9a827f5b5c5d9edb
Marko Kazimirovic <55856557+yepistream@users.noreply.github.com>
2025-07-24 11:45:57 +0200

    Update index.js

--- src/module/index.js
+++ src/module/index.js
@@ -1,5 +1,5 @@
 export { default as Cell } from './cell.js';
-export { default as Main } from './main.js';
+export { default as JThree} from './main.js';
 export * from './artist.js';
 export * from './NoScope.js';
 export * from './Train.js';
